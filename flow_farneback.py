645e3f45e69137e94f65ed4c0461a208997e38a9
MauroOrlic <mauro.orlic@gmail.com>
2020-09-05 20:00:48 +0200

    Minor refactoring, fixing comments.

--- flow_farneback.py
+++ flow_farneback.py
@@ -1,29 +1,39 @@
 from typing import Generator
 
 import numpy as np
-from cv2 import VideoCapture, cvtColor, COLOR_BGR2GRAY, calcOpticalFlowFarneback, cartToPolar
+from cv2 import (
+    VideoCapture,
+    cvtColor,
+    COLOR_BGR2GRAY,
+    calcOpticalFlowFarneback,
+    cartToPolar
+)
 
 
 def init_frame(capture: VideoCapture):
     status, frame_raw = capture.read()
 
     if not status:
-        raise IOError('Failed to read frame from capture.')
+        raise IOError('Failed to read first frame from capture.')
 
     return frame_raw
 
 
 def generate_flow_from_capture(capture: VideoCapture, magnitude_threshold=2) -> Generator[np.ndarray, None, None]:
+    # Get first frame and convert it to grayscale
     frame_current = cvtColor(init_frame(capture), COLOR_BGR2GRAY)
 
     while True:
         frame_previous = frame_current
 
+        # Get new frame and check if capture is working (detects last frame in a video file).
         status, frame_current = capture.read()
         if not status:
             break
+        # Convert new frame to grayscale
         frame_current = cvtColor(frame_current, COLOR_BGR2GRAY)
 
+        # Calculate optical flow (movement) between the previous and current frame
         flow = calcOpticalFlowFarneback(
             prev=frame_previous,
             next=frame_current,
@@ -38,7 +48,7 @@ def generate_flow_from_capture(capture: VideoCapture, magnitude_threshold=2) -> 
         )
         magnitude, angle = cartToPolar(flow[..., 0], flow[..., 1])
 
-        # Ignore very small movements (magnitude is in range 0.0 - 100.0)
+        # Ignores very small movements by setting magnitude to 0 (magnitude is in range 0.0 - 100.0)
         magnitude[magnitude < magnitude_threshold] = 0.0
 
         yield magnitude, angle
